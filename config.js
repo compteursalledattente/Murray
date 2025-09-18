0f99179efdf3e58c7ef2d2c5f8e17cefefa0783a
compteursalledattente <compteursalledattente@gmail.com>
2025-09-18 19:41:52 -0400

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
-
-
-
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
